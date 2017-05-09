f482c64760b0d7e277e7e7c7f5f4fd20d62ace27
xxcclong <hkz@hkzdeMacBook-Air.local>
2017-05-09 12:50:12 +0800

    dasd
    Dasdasdas

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  first_c++_on_mac
+//
+//  Created by hkz on 2017/5/9.
+//  Copyright © 2017年 hkz. All rights reserved.
+//
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=1;
+    cout<<a<<endl;
+}
